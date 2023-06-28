132e46be002ea3cb7f7469aa3ba75946708e3e46
MSI\chris <CristianDMZ>
2023-06-27 18:31:02 -0600

    guardars

---
+++ pagina-main/Database/conexcion.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "paginamain";
+
+// Crear una conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+?>
+
