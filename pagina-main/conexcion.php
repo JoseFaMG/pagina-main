09e4687373a2fa01c2282e6a368affa5d7db1c21
MSI\chris <CristianDMZ>
2023-06-27 19:31:20 -0600

    guardar hack

--- pagina-main/conexcion.php
+++ pagina-main/conexcion.php
@@ -1,11 +1,27 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$database = "paginamain";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$database = "tu_base_de_datos";
 
-// Crear una conexión
-$conn = new mysqli($servername, $username, $password, $database);
+// Establecer la conexión
+$connection = mysqli_connect($servername, $username, $password, $database);
 
+// Verificar la conexión
+if (!$connection) {
+    die("Error al conectar a la base de datos: " . mysqli_connect_error());
+}
 
+// Ejecutar consulta
+$query = "SELECT * FROM usuarios";
+$result = mysqli_query($connection, $query);
+
+// Obtener los resultados como un array
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Enviar los resultados como respuesta en formato JSON
+echo json_encode($rows);
+
+// Cerrar la conexión
+mysqli_close($connection);
 ?>
--- pagina-main/estructura.js
+++ pagina-main/estructura.js
@@ -1,14 +1,38 @@
-function validation(){
-    var N,E,P,B,A,X;
-    N=document.getElementById('Name').value;
-    E=document.getElementById('Email').value;
-    P=document.getElementById('Phone').value;
-    B=document.getElementById('BookingDate').value;
-    A=document.getElementById('Aadhar').value;
-    if (N==' ' || E==' ' || P==' ' || B==' ' || A==' '){
-        alert("Porfavor rellena las casillas faltantes");
-    }else{
-        alert("Tu registro se envio correctamente");
+function validation() {
+    var N, E, P, B, A;
+    N = document.getElementById('Name').value;
+    E = document.getElementById('Email').value;
+    P = document.getElementById('Phone').value;
+    B = document.getElementById('BookingDate').value;
+    A = document.getElementById('Aadhar').value;
+    
+    if (N.trim() === '' || E.trim() === '' || P.trim() === '' || B.trim() === '' || A.trim() === '') {
+        alert("Por favor, completa todos los campos.");
+    } else {
+        // Crear un objeto de datos a enviar al servidor
+        var data = {
+            name: N,
+            email: E,
+            phone: P,
+            bookingDate: B,
+            aadhar: A
+        };
+        
+        // Realizar la solicitud AJAX
+        $.ajax({
+            url: "conexion.php", // Ruta al archivo PHP en tu servidor
+            type: "POST",
+            data: data,
+            success: function(response) {
+                // Manipular la respuesta del servidor
+                alert("Tu registro se envió correctamente.");
+            },
+            error: function(xhr, status, error) {
+                // Manejar errores de la solicitud
+                console.error(error);
+                alert("Ocurrió un error al enviar el registro.");
+            }
+        });
     }
-
 }
+
